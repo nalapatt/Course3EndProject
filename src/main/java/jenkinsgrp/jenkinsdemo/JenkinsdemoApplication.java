91018174a322880ba338ee0e39d7c1c95ca95f0a
Ubuntu <ubuntu@ip-172-31-41-139.ec2.internal>
2020-12-09 02:14:06 +0000

    Added logic and test

--- src/main/java/jenkinsgrp/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/jenkinsgrp/jenkinsdemo/JenkinsdemoApplication.java
@@ -1,4 +1,23 @@
-package jenkinsgrp.jenkinsdemo;
+package com.simplilearn.ticketGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketGeneratorApplication {
+    public static double getRandomTicketNumber(){
+   	 double min=1000;
+   	 double max=9999;
+   		 double x = (int)(Math.random()*((max-min)+1))+min;
+   		 return x;
+    }
+    public static void main(String[] args) {
+   	 SpringApplication.run(TicketGeneratorApplication.class, args);
+    }
+
+}
+/*package jenkinsgrp.jenkinsdemo;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,4 +29,4 @@ public class JenkinsdemoApplication {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 	}
 
-}
+}*/
--- src/test/java/jenkinsgrp/jenkinsdemo/JenkinsdemoApplicationTests.java
+++ src/test/java/jenkinsgrp/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -1,4 +1,4 @@
-package jenkinsgrp.jenkinsdemo;
+/*package jenkinsgrp.jenkinsdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,21 @@ class JenkinsdemoApplicationTests {
 	void contextLoads() {
 	}
 
+}*/
+package com.simplilearn.ticketGenerator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGeneratorApplicationTests {
+
+    private TicketGeneratorApplication ticketer = new TicketGeneratorApplication();
+    @Test
+    public void testRandomNumber() {
+   	 assertTrue(ticketer.getRandomTicketNumber()>1000&&ticketer.getRandomTicketNumber()<9999);
+    }
+
 }
+
